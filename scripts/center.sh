b9b2634dd2c34484cb454e7d70b05742d2d64941
Juan <leptinox@gmail.com>
2018-06-22 16:51:08 -0500

    Added custom scripts files

---
+++ scripts/center.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+unset x y w h
+eval $(xwininfo -id $(xdotool getactivewindow) |
+    sed -n -e "s/^ \+Absolute upper-left X: \+\([0-9]\+\).*/x=\1/p" \
+           -e "s/^ \+Absolute upper-left Y: \+\([0-9]\+\).*/y=\1/p" \
+           -e "s/^ \+Width: \+\([0-9]\+\).*/w=\1/p" \
+           -e "s/^ \+Height: \+\([0-9]\+\).*/h=\1/p" )
+
+let a=1920/2-w/2
+let b=1080/2-h/2
+
+wmctrl -r :ACTIVE: -e 1,$a,$b,-1,-1
+
+exit 0
